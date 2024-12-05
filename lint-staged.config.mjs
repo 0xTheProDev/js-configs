5e4a3078f632c8e440e42a90c3cc975ba16ff1db
Progyan Bhattacharya <bprogyan@gmail.com>
2024-12-05 23:55:50 +0530

    chore: Upgrade Dependencies for ESLint and TSUp
    Signed-off-by: Progyan Bhattacharya <bprogyan@gmail.com>

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,5 @@
 // This configuration is applied from the workspace root.
-module.exports = {
+export default {
   "package.json": ["sort-package-json", "check-yarn-lock"],
   "yarn.lock": ["check-yarn-lock"],
   "*.{cjs,js,mjs,ts}": ["prettier --write", "eslint --fix"],
